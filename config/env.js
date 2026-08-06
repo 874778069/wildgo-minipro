5dcef5206c4cea2dc19f989ab5264c243e0038a5
zhuanglinan <874778069@qq.com>
2026-08-06 16:15:42 +0800

    更新开发和生产环境的 baseUrl 为 https://www.fdzhuang.site

--- config/env.js
+++ config/env.js
@@ -2,10 +2,10 @@ const ENV = 'development'
 
 const environments = {
   development: {
-    baseUrl: 'http://106.55.255.101:3000'
+    baseUrl: 'https://www.fdzhuang.site'
   },
   production: {
-    baseUrl: 'https://api.fdzhuang.site'
+    baseUrl: 'https://www.fdzhuang.site'
   },
   // 如需单独联调测试环境，改这里并把 ENV 切到 test。
   test: {
